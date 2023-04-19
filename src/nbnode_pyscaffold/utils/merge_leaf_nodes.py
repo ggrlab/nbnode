cb14c3f4596aa0fe205ce1cb273b8d9161bf38b4
gunthergl_r2 <gunthergl@gmx.net>
2023-04-19 10:27:50 +0200

    add first part of the code

---
+++ src/nbnode_pyscaffold/utils/merge_leaf_nodes.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from nbnode_pyscaffold.nbnode import NBNode
+
+
+def merge_leaf_nodes(intermediate_node: NBNode, leaf_nodes_df: pd.DataFrame) -> float:
+    return leaf_nodes_df.loc[
+        [x.startswith(intermediate_node) for x in leaf_nodes_df.index]
+    ].sum()
