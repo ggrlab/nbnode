957c0a11c7ac2ce56f9739df74ede3c1cd67ee8e
gunthergl_r2 <gunthergl@gmx.net>
2023-04-25 13:41:10 +0200

    test frame_cov

---
+++ tests/utils/test_nbnode_util.py
@@ -0,0 +1,11 @@
+import datatable as dt
+import numpy as np
+import pandas as pd
+
+from nbnode_pyscaffold.nbnode_util import frame_cov
+
+def test_frame_cov():
+    dt_frame = dt.Frame(np.random.rand(10, 5))
+    pd_covmat = pd.DataFrame(dt_frame.to_numpy()).cov()
+    dt_covmat = frame_cov(dt_frame)
+    assert np.allclose(pd_covmat.to_numpy(), dt_covmat.to_numpy())
