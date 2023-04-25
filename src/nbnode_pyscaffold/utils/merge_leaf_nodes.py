8cef10ad3a7982cf6a70893e4aed38b9ac397215
gunthergl_r2 <gunthergl@gmx.net>
2023-04-25 10:19:14 +0200

    doc merge_leaf_nodes

--- src/nbnode_pyscaffold/utils/merge_leaf_nodes.py
+++ src/nbnode_pyscaffold/utils/merge_leaf_nodes.py
@@ -1,9 +1,28 @@
 import pandas as pd
 
-from nbnode_pyscaffold.nbnode import NBNode
 
+def merge_leaf_nodes(intermediate_node: str, leaf_nodes_df: pd.DataFrame) -> float:
+    """Merge leaf node dataframe into intermediate node.
 
-def merge_leaf_nodes(intermediate_node: NBNode, leaf_nodes_df: pd.DataFrame) -> float:
+    Args:
+        intermediate_node (str):
+            The (full) name of the intermediate node to merge leaf nodes into.
+        leaf_nodes_df (pd.DataFrame):
+            The dataframe of leaf nodes to merge into the intermediate node.
+            Its index are the full names of the leaf nodes. The names are organized in
+            a hierarchy, separated by "/". Therefore, any intermediate node is a prefix
+            of any of its leaf nodes.
+
+            For some values it is useful to have the sum of all leaf nodes, especially
+            for
+                - Dirichlet concentration parameters
+                - Means of cell proportions
+                - Numbers of cells
+
+    Returns:
+        float:
+            Sum of all leaf nodes whose names start with the intermediate node name
+    """
     return leaf_nodes_df.loc[
         [x.startswith(intermediate_node) for x in leaf_nodes_df.index]
     ].sum()
