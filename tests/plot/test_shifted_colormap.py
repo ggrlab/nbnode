ad42abceaa7a3d0ffe9680fd0f28b8590a504b5c
gunthergl_r2 <gunthergl@gmx.net>
2023-04-25 13:56:24 +0200

    shifted_colormap test

---
+++ tests/plot/test_shifted_colormap.py
@@ -0,0 +1,25 @@
+def test_shifted_colormap():
+    import matplotlib
+    from nbnode_pyscaffold.plot.shifted_colormap import shifted_colormap
+
+    new_cmap = shifted_colormap(
+        cmap=matplotlib.cm.RdBu_r,
+        min_val=-5,
+        max_val=10,
+        name="NoName",
+    )
+
+
+def test_linear_shifted_colormap():
+    import matplotlib
+    from nbnode_pyscaffold.plot.utils import LinearShiftedColormap
+
+    LinearShiftedColormap()
+    LinearShiftedColormap(range_min=-1, range_max=1)
+    LinearShiftedColormap(range_min=-1, range_max=1, base_cmap=matplotlib.cm.RdBu_r)
+    LinearShiftedColormap(
+        range_min=-1, range_max=1, base_cmap=matplotlib.cm.RdBu_r, name="shifted"
+    )
+    LinearShiftedColormap(
+        range_min=-10, range_max=10, base_cmap=matplotlib.cm.RdBu_r, name="shifted"
+    )
