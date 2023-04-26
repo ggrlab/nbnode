959c8f21b273759ce1ef49008a911e4ccff59ce7
gunthergl_r2 <gunthergl@gmx.net>
2023-04-26 17:57:12 +0200

    test all nbnode_trees if they are callable and pretty_print()able

---
+++ tests/test_nbnode_trees.py
@@ -0,0 +1,9 @@
+import nbnode_pyscaffold.nbnode_trees as nbtree
+
+
+def test_trees_init():
+    for x in nbtree.__dict__.keys():
+        if x.startswith("tree_"):
+            assert callable(nbtree.__dict__[x])
+            mytree = nbtree.__dict__[x]()
+            mytree.pretty_print()
