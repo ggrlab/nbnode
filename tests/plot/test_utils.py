79fa965c6b3d87c9145526de4cdcffd6a82e1227
gunthergl_r2 <gunthergl@gmx.net>
2023-04-26 17:57:19 +0200

    add test_plot_save_unified_dot

--- tests/plot/test_utils.py
+++ tests/plot/test_utils.py
@@ -8,6 +8,37 @@ def test_flatten():
     assert [1, 2, 3, "4", {5: 5}] == flatten([[1, [[2]], [[[3]]]], [["4"], {5: 5}]])
     assert flatten([[1, 2, 3, 4, 5, 6, 7, 8]]) == [1, 2, 3, 4, 5, 6, 7, 8]
 
-def test_plot_save_unified():
-    # Is used all around in test_NBNode.py
-    pass
+def test_plot_save_unified_dot():
+    import os
+    import nbnode_pyscaffold.nbnode_trees as nbtree
+    import anytree.exporter as a_exp
+    import pydotplus
+    from nbnode_pyscaffold.plot.utils import plot_save_unified
+
+    simpletree = nbtree.tree_simple()
+    dot_data = a_exp.UniqueDotExporter(
+        simpletree,
+        options=['node [shape=box, style="filled", color="black"];'],
+        nodeattrfunc=lambda node: 'label="{}", fillcolor="white"'.format(node.name),
+    )
+    dotdata_str = "\n".join([x for x in dot_data])
+    # print(dotdata_str)
+    graph: pydotplus.Dot = pydotplus.graph_from_dot_data(dotdata_str)
+    plot_save_unified(
+        any_plot=graph, file="tests_output/pydotplus_graph_nocolor_NOTpdf.NOTpdf"
+    )
+    assert os.path.exists("tests_output/pydotplus_graph_nocolor_NOTpdf.pdf")
+
+    plot_save_unified(
+        any_plot=graph, file="tests_output/pydotplus_graph_nocolor.pdf"
+    )
+
+def test_plot_save_unified_matplotlib():
+    import os
+    import matplotlib.pyplot as plt
+    from nbnode_pyscaffold.plot.utils import plot_save_unified
+
+    plt.plot([1, 2, 3, 4])
+    plot_save_unified(any_plot=plt, file="tests_output/matplotlib_graph.png")
+    assert os.path.exists("tests_output/matplotlib_graph.png")
+    plt.close("all")
