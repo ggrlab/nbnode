3c84ede05b88f40afe016cf1d2b3e771d19a2d3c
gunthergl_r2 <gunthergl@gmx.net>
2023-04-28 14:34:03 +0200

    debugging_only_return_sampled_cell_numbers: remove "debugging"

--- src/nbnode_pyscaffold/simulation/TreeMeanRelative.py
+++ src/nbnode_pyscaffold/simulation/TreeMeanRelative.py
@@ -19,7 +19,7 @@ class TreeMeanRelative:
         verbose=True,
         seed_sample_0=129873,
         save_dir="sim/sim00_pure_estimate",
-        debugging_only_return_sampled_cell_numbers=True,
+        only_return_sampled_cell_numbers=False,
     ) -> None:
         self.flowsim_tree = flowsim_tree
         self.change_pop_mean_proportional = change_pop_mean_proportional
@@ -31,8 +31,8 @@ class TreeMeanRelative:
         self.save_dir = save_dir
         # The following variable should only be turned off if you do NOT want the cells actually
         # sampled, but instead want to return only the cell NUMBERS per population
-        self._debugging_only_return_sampled_cell_numbers = (
-            debugging_only_return_sampled_cell_numbers
+        self._only_return_sampled_cell_numbers = (
+            only_return_sampled_cell_numbers
         )
 
     @staticmethod
@@ -45,7 +45,7 @@ class TreeMeanRelative:
         verbose=True,
         seed_sample_0=129873,
         save_dir="sim/sim00_pure_estimate",
-        _debugging_only_return_sampled_cell_numbers=True,
+        _only_return_sampled_cell_numbers=False,
     ) -> Tuple[pd.DataFrame, Dict[str, Any], List[pd.DataFrame]]:
         if isinstance(flowsim_tree, str):
             with open(
@@ -67,7 +67,7 @@ class TreeMeanRelative:
             change_pop_mean_proportional=change_pop_mean_proportional,
             seed_sample_0=seed_sample_0,
             save_dir=save_dir,
-            only_return_sampled_cell_numbers=_debugging_only_return_sampled_cell_numbers,
+            only_return_sampled_cell_numbers=_only_return_sampled_cell_numbers,
         )
         if save_dir is not None:
             true_popcounts.to_csv(f"{save_dir}.csv")
@@ -86,7 +86,7 @@ class TreeMeanRelative:
             verbose=self.verbose,
             seed_sample_0=self.seed_sample_0,
             save_dir=self.save_dir,
-            _debugging_only_return_sampled_cell_numbers=self._debugging_only_return_sampled_cell_numbers,
+            _only_return_sampled_cell_numbers=self._only_return_sampled_cell_numbers,
         )
 
     def sample_customize(
@@ -98,7 +98,7 @@ class TreeMeanRelative:
         verbose=None,
         seed_sample_0=None,
         save_dir=None,
-        _debugging_only_return_sampled_cell_numbers=None,
+        _only_return_sampled_cell_numbers=None,
     ):
         return self._sample(
             n_samples=self.n_samples if n_samples is None else n_samples,
@@ -114,7 +114,7 @@ class TreeMeanRelative:
             if seed_sample_0 is None
             else seed_sample_0,
             save_dir=self.save_dir if save_dir is None else save_dir,
-            _debugging_only_return_sampled_cell_numbers=self._debugging_only_return_sampled_cell_numbers
-            if _debugging_only_return_sampled_cell_numbers is None
-            else _debugging_only_return_sampled_cell_numbers,
+            _only_return_sampled_cell_numbers=self._only_return_sampled_cell_numbers
+            if _only_return_sampled_cell_numbers is None
+            else _only_return_sampled_cell_numbers,
         )
