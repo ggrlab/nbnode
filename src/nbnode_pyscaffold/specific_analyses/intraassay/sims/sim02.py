33af08d25d5e327df21fe55e6d4d54c4a903c5aa
gunthergl_r2 <gunthergl@gmx.net>
2023-04-28 15:17:05 +0200

    debugging_only_return_sampled_cell_numbers: remove debugging, generate() -> sample()

--- src/nbnode_pyscaffold/specific_analyses/intraassay/sims/sim02.py
+++ src/nbnode_pyscaffold/specific_analyses/intraassay/sims/sim02.py
@@ -12,7 +12,7 @@ def sim02_temra(
     verbose=True,
     seed_sample_0=129873,
     save_dir="sim/sim02_temra",
-    debugging_only_return_sampled_cell_numbers=False,
+    only_return_sampled_cell_numbers=False,
 ):
     proportional_generator = TreeMeanRelative(
         change_pop_mean_proportional={
@@ -26,6 +26,6 @@ def sim02_temra(
         verbose=verbose,
         seed_sample_0=seed_sample_0,
         save_dir=save_dir,
-        debugging_only_return_sampled_cell_numbers=debugging_only_return_sampled_cell_numbers,
+        only_return_sampled_cell_numbers=only_return_sampled_cell_numbers,
     )
-    return proportional_generator.generate()
+    return proportional_generator.sample()
