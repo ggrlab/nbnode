12f264f591169f5578541852b46a92cb8018e317
gunthergl_r2 <gunthergl@gmx.net>
2023-05-03 12:21:06 +0200

    download zip not all .csvs

---
+++ tests/specific_analyses/e03_download_intraassay_zenodo_zip.py
@@ -0,0 +1,31 @@
+# From https://github.com/zenodo/zenodo/issues/1888
+import os
+import zipfile
+import io
+import requests
+
+ACCESS_TOKEN = "ZENODO_ACCESS_TOKEN"
+record_id = "7890571"
+
+r = requests.get(
+    f"https://zenodo.org/api/records/{record_id}", params={"access_token": ACCESS_TOKEN}
+)
+print(r.json())
+download_urls = [f["links"]["self"] for f in r.json()["files"]]
+filenames = [f["key"] for f in r.json()["files"]]
+# outdir = "example_data/asinh.align_manual.CD3_Gate"
+# os.makedirs(outdir, exist_ok=True)
+
+# for filename, url in zip(filenames, download_urls):
+#     print("Downloading:", filename)
+#     r = requests.get(url, params={"access_token": ACCESS_TOKEN})
+#     with open(os.path.join(outdir, filename), "wb") as f:
+#         f.write(r.content)
+
+
+my_zipfile = [(fn, url) for fn, url in zip(filenames, download_urls) if fn.endswith(".zip")][0]
+print("Downloading:", my_zipfile[0])
+r = requests.get(my_zipfile[1], params={"access_token": ACCESS_TOKEN})
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall("example_data")
+
